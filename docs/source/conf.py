4349df5a992453273b5dacf6e1f5046d9c742b60
Alina <alinaterianyk22@gmail.com>
2026-03-26 19:04:57 +0200

    docs: generate HTML documentation

---
+++ docs/source/conf.py
@@ -0,0 +1,32 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Handwritten Recognition'
+copyright = '2026, Alina Terianyk'
+author = 'Alina Terianyk'
+release = 'n'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["sphinx.ext.autodoc"]
+
+import os, sys
+sys.path.insert(0, os.path.abspath("../.."))
+autodoc_mock_imports = ["streamlit", "cv2", "numpy", "tensorflow"]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'en'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
